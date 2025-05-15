fdf04e5abdd2479d045466853a098659ebc7794e
umesh <umeshsahijwani88@gmail.com>
2025-05-16 04:51:22 +0530

    added definations of functionality

--- Pages/GoogleFormPage.py
+++ Pages/GoogleFormPage.py
@@ -6,9 +6,30 @@ class GoogleFormLocator:
     dropDownState = "//div[@role= 'option']//span[text()= 'Choose'][1]"
     txtAreaOffice = "//span[contains(text(), '{}')]//ancestor::div[@jscontroller]//input"
     btnSubmit = "//span[text() = 'Submit']"
+    btnSwitchAccount = "//a[text()= 'Switch accounts']"
+    currentAccount = "//a[text()= 'Switch accounts']/parent::div//span"
 
 class GoogleFormPage(CommonFunction):
     def select_dropdown(self, field, value):
         self.click_element(By.XPATH, f"//span[contains(text(), '{field}')]//ancestor::div[@jscontroller]//span[text() = 'Choose']")
         self.click_element(By.XPATH, f"//span[contains(text(), '{field}')]//ancestor::div[@jscontroller]//div[@role= 'option' and @data-value = '{value}']")
         time.sleep(2)
+
+    def enter_value_in_textbox(self, label, value):
+        self.type_element(By.XPATH, f"//span[contains(text(), '{label}')]//ancestor::div[@jscontroller]//input", value)
+
+    def submit_form(self):
+        self.click_element(By.XPATH, GoogleFormLocator.btnSubmit)
+
+    def submit_another_response(self):
+        pass
+
+    def get_all_accounts_displayed(self):
+        pass
+
+    def get_selected_google_account(self):
+        pass
+
+    def switch_account(self, email):
+        self.click_element(By.XPATH, GoogleFormLocator.btnSwitchAccount)
+
